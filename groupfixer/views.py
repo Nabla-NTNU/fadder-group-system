43718ea232491cf68b5c4d64786b4fef70688c2f
kristoea <kristoea@stud.ntnu.no>
2021-02-24 21:42:41 +0100

    Fikser lisens for kodesnutt
    Mens vi er i lisenshjørnet så kom jeg på at jeg kopierte denne reCAPTCHA-integrasjonen fra en guide jeg fant på nettet. Den når kanskje ikke terskelen for hva som egentlig dekkes av opphavsrett, men hvorfor ikke være sikker? Fun fact: kodesnutter fra Stack Overflow er i utgangspunktet under CC BY-SA 4.0, altså copyleft ala GPL! Det beste er nok å ikke kopiere verbatim og heller endre den litt. Denne spesifikke snutten har noen kosmetiske endringer, men jeg tror ikke det egentlig er nok. Snutten var dessuten ikke fra SO, men heller under MIT lisens, som man kan bruke i GPL prosjekter, men ikke omvendt! Mye å holde styr på...

--- groupfixer/views.py
+++ groupfixer/views.py
@@ -48,7 +48,24 @@ def post_choices(http_request):
         return HttpResponseRedirect(reverse('groupfixer:main'))
 
     try:
-        ''' Begin reCAPTCHA validation '''
+        '''
+        Begin reCAPTCHA validation
+
+        MIT License
+
+        Copyright (c) 2017 Simple is Better Than Complex
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        '''
         recaptcha_response = http_request.POST.get('g-recaptcha-response')
         url = 'https://www.google.com/recaptcha/api/siteverify'
         values = {
